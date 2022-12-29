bed8f56d9a193b4b1100109f0ca2671434da8332
essare-rimaz <71963709+essare-rimaz@users.noreply.github.com>
2022-12-29 19:46:28 +0100

    replace last name

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Ngubáčkovi',
+    title: 'Mičánkovi',
     htmlAttrs: {
       lang: 'cs'
     },
