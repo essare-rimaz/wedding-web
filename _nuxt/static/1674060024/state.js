0d25076f9678b4115d6db6e3a1ea2bb68a3a065e
essare-rimaz <essare-rimaz@users.noreply.github.com>
2023-02-12 21:36:31 +0000

    deploy: 387eaa82422769d50bf073ba7bfa0c82e1ef477a

--- _nuxt/static/1674060024/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwedding-web\u002F_nuxt\u002Fstatic\u002F1674060024",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fwedding-web\u002F",assetsPath:"\u002Fwedding-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
