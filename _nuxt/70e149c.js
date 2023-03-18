5a8b18178bd7ff5ca5db75034d75d49513b7d1fe
essare-rimaz <essare-rimaz@users.noreply.github.com>
2023-03-18 14:34:28 +0000

    deploy: c1a3d1e81b63331cd2161e25183b5b130ff83808

---
+++ _nuxt/70e149c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{313:function(t,n,e){t.exports=e.p+"img/splitter.077009f.svg"},320:function(t,n,e){"use strict";e.r(n);var c={name:"SplitElement"},l=e(46),component=Object(l.a)(c,(function(){return(0,this._self._c)("img",{staticClass:"w-64 mt-3 mb-7",attrs:{src:e(313)}})}),[],!1,null,"c9752d08",null);n.default=component.exports}}]);
